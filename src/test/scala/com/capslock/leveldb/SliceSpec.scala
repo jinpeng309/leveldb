5876172c9cfe9e7d4fa90ae0dbd7be7f529456b0
alvin <alvin@liao.com>
2016-05-11 18:41:23 +0800

    refactoring

--- src/test/scala/com/capslock/leveldb/SliceSpec.scala
+++ src/test/scala/com/capslock/leveldb/SliceSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.{FlatSpec, _}
  * Created by capslock.
  */
 class SliceSpec extends FlatSpec with Matchers {
-    "Slice construct" should "" in {
+    "Slice construct from a byte array " should " length and offset should be matched" in {
         val slice = Slice(Array.fill(3)(0.toByte))
         slice.offset shouldBe 0
         slice.length shouldBe 3
