9bc4b60f1e2adb5d44dd5c6e7cc48e673a49f46c
alvin <alvin@liao.com>
2016-05-12 17:40:53 +0800

    refactoring

--- src/main/scala/com/capslock/leveldb/DynamicSliceOutput.scala
+++ src/main/scala/com/capslock/leveldb/DynamicSliceOutput.scala
@@ -18,7 +18,7 @@ class DynamicSliceOutput(estimatedSize: Int) extends SliceOutput {
     override def writeBytes(source: Slice, length: Int): Unit = writeBytes(source, 0, length)
 
     override def writeBytes(source: Slice, index: Int, length: Int): Unit = {
-        sliceData = ensureSize(sliceData, size + length)
+        ensureSize(size + length)
         sliceData.setBytes(size, source, index, length)
         size += length
     }
@@ -26,26 +26,26 @@ class DynamicSliceOutput(estimatedSize: Int) extends SliceOutput {
     override def writeBytes(source: Array[Byte]): Unit = writeBytes(source, 0, source.length)
 
     override def writeBytes(source: Array[Byte], sourceIndex: Int, length: Int): Unit = {
-        sliceData = ensureSize(sliceData, size + length)
+        ensureSize(size + length)
         sliceData.setBytes(size, source, sourceIndex, length)
         size += length
     }
 
     override def writeBytes(source: ByteBuffer): Unit = {
         val length = source.remaining
-        sliceData = ensureSize(sliceData, size + length)
+        ensureSize(size + length)
         sliceData.setBytes(size, source)
         size += length
     }
 
     override def writeBytes(source: InputStream, length: Int): Unit = {
-        sliceData = ensureSize(sliceData, size + length)
+        ensureSize(size + length)
         sliceData.setBytes(size, source, length)
         size += length
     }
 
     override def writeBytes(source: FileChannel, position: Int, length: Int): Unit = {
-        sliceData = ensureSize(sliceData, size + length)
+        ensureSize(size + length)
         sliceData.setBytes(size, source, position, length)
         size += length
     }
@@ -58,7 +58,7 @@ class DynamicSliceOutput(estimatedSize: Int) extends SliceOutput {
 
     override def writeZero(length: Int): Unit = {
         if (length > 0) {
-            sliceData = ensureSize(sliceData, size + length)
+            ensureSize(size + length)
             val nLong = length >>> 8
             val nBytes = length & 7
             for (i <- 0 until nLong) {
@@ -77,40 +77,40 @@ class DynamicSliceOutput(estimatedSize: Int) extends SliceOutput {
     }
 
     override def writeShort(value: Int): Unit = {
-        sliceData = ensureSize(sliceData, size + 2)
+        ensureSize(size + 2)
         sliceData.setShort(size, value)
         size += 2
     }
 
     override def writeInt(value: Int): Unit = {
-        sliceData = ensureSize(sliceData, size + 4)
+        ensureSize(size + 4)
         sliceData.setInt(size, value)
         size += 4
     }
 
     override def writeLong(value: Long): Unit = {
-        sliceData = ensureSize(sliceData, size + 8)
+        ensureSize(size + 8)
         sliceData.setLong(size, value)
         size += 8
     }
 
     override def writeByte(value: Int): Unit = {
-        sliceData = ensureSize(sliceData, size + 1)
+        ensureSize(size + 1)
         sliceData.setByte(size, value)
         size += 1
     }
 
-    def ensureSize(slice: Slice, minWritableSize: Int): Slice = {
-        slice.length match {
-            case _ if slice.length >= minWritableSize => slice
-            case _ if slice.length < minWritableSize =>
-                val minCapability = slice.length + minWritableSize
-                var newCapability = if (slice.length == 0) 1 else slice.length
+    def ensureSize(minWritableSize: Int): Unit = {
+        sliceData.length match {
+            case _ if sliceData.length >= minWritableSize => slice
+            case _ if sliceData.length < minWritableSize =>
+                val minCapability = sliceData.length + minWritableSize
+                var newCapability = if (sliceData.length == 0) 1 else sliceData.length
                 while (newCapability < minCapability) {
                     newCapability <<= 1
                 }
                 val result = Slice(newCapability)
-                result.setBytes(0, slice, 0, slice.length)
+                result.setBytes(0, sliceData, 0, sliceData.length)
                 result
         }
     }
