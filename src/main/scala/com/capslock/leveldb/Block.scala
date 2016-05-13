2461b83cf3229b54319faebc98a6faede728a2b8
capslock <capslock1874@gmail.com>
2016-05-13 22:50:07 +0800

    refactoring

--- src/main/scala/com/capslock/leveldb/Block.scala
+++ src/main/scala/com/capslock/leveldb/Block.scala
@@ -1,9 +1,12 @@
 package com.capslock.leveldb
 
+import java.util.Comparator
+
 /**
  * Created by capslock.
  */
 class Block extends SeekingIterable[Slice, Slice] {
-    override def iterator: SeekingIterator[Slice, Slice] = ???
-
+    override def iterator(): SeekingIterator[Slice, Slice] = BlockIterator(Slice(3), Slice(3), new Comparator[Slice] {
+        override def compare(o1: Slice, o2: Slice): Int = 1
+    } )
 }
