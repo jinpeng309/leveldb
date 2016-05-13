bc3904b62a9668609c7e2ad89e51b47837d316c8
capslock <capslock1874@gmail.com>
2016-05-13 23:27:59 +0800

    refactoring

--- src/main/scala/com/capslock/leveldb/Block.scala
+++ src/main/scala/com/capslock/leveldb/Block.scala
@@ -9,23 +9,9 @@ import com.capslock.leveldb.SizeOf._
  */
 class Block(block: Slice, comparator: Comparator[Slice]) extends SeekingIterable[Slice, Slice] {
     val restartCount = block.getInt(block.length - SizeOf.SIZE_OF_INT)
-    val data = {
-        if (restartCount > 0) {
-            val restartOffset = block.length - SIZE_OF_INT * (1 + restartCount)
-            block.slice(0, restartOffset)
-        } else {
-            Slice.empty
-        }
-    }
-    val restartPositions = {
-        if (restartCount > 0) {
-            val restartOffset = block.length - SIZE_OF_INT * (1 + restartCount)
-            block.slice(restartOffset, restartCount * SIZE_OF_INT)
-        } else {
-            Slice.empty
-        }
-    }
-
+    val restartOffset = block.length - SIZE_OF_INT * (1 + restartCount)
+    val data = if (restartCount > 0) block.slice(0, restartOffset) else Slice.empty
+    val restartPositions = if (restartCount > 0) block.slice(restartOffset, restartCount * SIZE_OF_INT) else Slice.empty
 
     override def iterator(): SeekingIterator[Slice, Slice] = BlockIterator(data, restartPositions, comparator)
 }
