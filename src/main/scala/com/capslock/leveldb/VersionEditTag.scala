3272b2cfca2399553f41d7798df0883f3192d623
alvin <alvin@liao.com>
2016-05-24 18:20:06 +0800

    improve test case and fix bug

--- src/main/scala/com/capslock/leveldb/VersionEditTag.scala
+++ src/main/scala/com/capslock/leveldb/VersionEditTag.scala
@@ -11,156 +11,158 @@ sealed abstract class VersionEditTag(val persistentId: Int) {
     def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit)
 }
 
-case object COMPARATOR extends VersionEditTag(1) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-        val bytes = Array.fill[Byte](VariableLengthQuantity.readVariableLengthInt(sliceInput))(0)
-        sliceInput.readBytes(bytes)
-        versionEdit.comparatorName = Option[String](new String(bytes, Charsets.UTF_8))
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        for (comparatorName <- versionEdit.comparatorName) {
-            VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-            val bytes = comparatorName.getBytes(Charsets.UTF_8)
-            VariableLengthQuantity.writeVariableLengthInt(bytes.length, sliceOutput)
-            sliceOutput.writeBytes(bytes)
-        }
-    }
-}
-
-case object LOG_NUMBER extends VersionEditTag(2) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-        versionEdit.logNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        for (logNumber <- versionEdit.logNumber) {
-            VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-            VariableLengthQuantity.writeVariableLengthLong(logNumber, sliceOutput)
-        }
-    }
-}
-
-case object PREVIOUS_LOG_NUMBER extends VersionEditTag(9) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-        versionEdit.previousLogNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        for (previousLogNumber <- versionEdit.previousLogNumber) {
-            VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-            VariableLengthQuantity.writeVariableLengthLong(previousLogNumber, sliceOutput)
-        }
-    }
-}
-
-case object NEXT_FILE_NUMBER extends VersionEditTag(3) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-        versionEdit.nextFileNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        for (nextFileNumber <- versionEdit.nextFileNumber) {
-            VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-            VariableLengthQuantity.writeVariableLengthLong(nextFileNumber, sliceOutput)
-        }
-    }
-}
-
-case object LAST_SEQUENCE extends VersionEditTag(4) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-        versionEdit.lastSequenceNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        for (nextFileNumber <- versionEdit.lastSequenceNumber) {
-            VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-            VariableLengthQuantity.writeVariableLengthLong(nextFileNumber, sliceOutput)
-        }
-    }
-}
-
-case object COMPACT_POINTER extends VersionEditTag(5) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-        import InternalKey.SliceToInternalKeyImplicit
-        val level = VariableLengthQuantity.readVariableLengthInt(sliceInput)
-        val keySlice = sliceInput.readBytes(VariableLengthQuantity.readVariableLengthInt(sliceInput))
-        versionEdit.setCompactPoint(level, keySlice.toInternalKey)
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        import InternalKey.InternalKeyToSliceImplicit
-        versionEdit.compactPointers.foreach {
-            case (level: Int, key: InternalKey) =>
-                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-                VariableLengthQuantity.writeVariableLengthInt(level, sliceOutput)
-                val keySlice = key.toSlice
-                VariableLengthQuantity.writeVariableLengthInt(keySlice.length, sliceOutput)
-                sliceOutput.writeBytes(keySlice)
-
-        }
-    }
-}
-
-case object DELETED_FILE extends VersionEditTag(6) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        versionEdit.deleteFiles.foreach((entry: (Int, List[Long])) => {
-            val level = entry._1
-            val fileList = entry._2
-            fileList.foreach(fileNumber => {
-                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-                VariableLengthQuantity.writeVariableLengthInt(level, sliceOutput)
-                VariableLengthQuantity.writeVariableLengthLong(fileNumber, sliceOutput)
-            })
-        })
-    }
-}
-
-case object NEW_FILE extends VersionEditTag(7) {
-    override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
-        import InternalKey.SliceToInternalKeyImplicit
-        val level = VariableLengthQuantity.readVariableLengthInt(sliceInput)
-        val fileNumber = VariableLengthQuantity.readVariableLengthLong(sliceInput)
-        val fileSize = VariableLengthQuantity.readVariableLengthLong(sliceInput)
-
-        val smallestKeyLength = VariableLengthQuantity.readVariableLengthInt(sliceInput)
-        val smallestKey = sliceInput.readBytes(smallestKeyLength).toInternalKey
-        val largestKeyLength = VariableLengthQuantity.readVariableLengthInt(sliceInput)
-        val largestKey = sliceInput.readBytes(largestKeyLength).toInternalKey
-
-        versionEdit.addFile(level, fileNumber, fileSize, smallestKey, largestKey)
-    }
-
-    override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
-        import InternalKey.InternalKeyToSliceImplicit
-        versionEdit.newFiles.foreach((entry: (Int, List[FileMetaData])) => {
-            val level = entry._1
-            val fileList = entry._2
-            fileList.foreach(fileMetaData => {
-                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
-                VariableLengthQuantity.writeVariableLengthInt(level, sliceOutput)
-
-                VariableLengthQuantity.writeVariableLengthLong(fileMetaData.fileNumber, sliceOutput)
-                VariableLengthQuantity.writeVariableLengthLong(fileMetaData.fileSize, sliceOutput)
-
-                val smallestKeySlice = fileMetaData.smallest.toSlice
-                val largestKeySlice = fileMetaData.largest.toSlice
-
-                VariableLengthQuantity.writeVariableLengthInt(smallestKeySlice.length, sliceOutput)
-                sliceOutput.writeBytes(smallestKeySlice)
-
-                VariableLengthQuantity.writeVariableLengthInt(largestKeySlice.length, sliceOutput)
-                sliceOutput.writeBytes(largestKeySlice)
-            })
-        })
-    }
-}
-
 object VersionEditTag {
+
+    case object COMPARATOR extends VersionEditTag(1) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+            val bytes = Array.fill[Byte](VariableLengthQuantity.readVariableLengthInt(sliceInput))(0)
+            sliceInput.readBytes(bytes)
+            versionEdit.comparatorName = Some[String](new String(bytes, Charsets.UTF_8))
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            for (comparatorName <- versionEdit.comparatorName) {
+                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                val bytes = comparatorName.getBytes(Charsets.UTF_8)
+                VariableLengthQuantity.writeVariableLengthInt(bytes.length, sliceOutput)
+                sliceOutput.writeBytes(bytes)
+            }
+        }
+    }
+
+    case object LOG_NUMBER extends VersionEditTag(2) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+            versionEdit.logNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            for (logNumber <- versionEdit.logNumber) {
+                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                VariableLengthQuantity.writeVariableLengthLong(logNumber, sliceOutput)
+            }
+        }
+    }
+
+    case object PREVIOUS_LOG_NUMBER extends VersionEditTag(9) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+            versionEdit.previousLogNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            for (previousLogNumber <- versionEdit.previousLogNumber) {
+                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                VariableLengthQuantity.writeVariableLengthLong(previousLogNumber, sliceOutput)
+            }
+        }
+    }
+
+    case object NEXT_FILE_NUMBER extends VersionEditTag(3) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+            versionEdit.nextFileNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            for (nextFileNumber <- versionEdit.nextFileNumber) {
+                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                VariableLengthQuantity.writeVariableLengthLong(nextFileNumber, sliceOutput)
+            }
+        }
+    }
+
+    case object LAST_SEQUENCE extends VersionEditTag(4) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+            versionEdit.lastSequenceNumber = Option(VariableLengthQuantity.readVariableLengthLong(sliceInput))
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            for (nextFileNumber <- versionEdit.lastSequenceNumber) {
+                VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                VariableLengthQuantity.writeVariableLengthLong(nextFileNumber, sliceOutput)
+            }
+        }
+    }
+
+    case object COMPACT_POINTER extends VersionEditTag(5) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+            import InternalKey.SliceToInternalKeyImplicit
+            val level = VariableLengthQuantity.readVariableLengthInt(sliceInput)
+            val keySlice = sliceInput.readBytes(VariableLengthQuantity.readVariableLengthInt(sliceInput))
+            versionEdit.setCompactPoint(level, keySlice.toInternalKey)
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            import InternalKey.InternalKeyToSliceImplicit
+            versionEdit.compactPointers.foreach {
+                case (level: Int, key: InternalKey) =>
+                    VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                    VariableLengthQuantity.writeVariableLengthInt(level, sliceOutput)
+                    val keySlice = key.toSlice
+                    VariableLengthQuantity.writeVariableLengthInt(keySlice.length, sliceOutput)
+                    sliceOutput.writeBytes(keySlice)
+
+            }
+        }
+    }
+
+    case object DELETED_FILE extends VersionEditTag(6) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            versionEdit.deleteFiles.foreach((entry: (Int, List[Long])) => {
+                val level = entry._1
+                val fileList = entry._2
+                fileList.foreach(fileNumber => {
+                    VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                    VariableLengthQuantity.writeVariableLengthInt(level, sliceOutput)
+                    VariableLengthQuantity.writeVariableLengthLong(fileNumber, sliceOutput)
+                })
+            })
+        }
+    }
+
+    case object NEW_FILE extends VersionEditTag(7) {
+        override def readValue(sliceInput: SliceInput, versionEdit: VersionEdit): Unit = {
+            import InternalKey.SliceToInternalKeyImplicit
+            val level = VariableLengthQuantity.readVariableLengthInt(sliceInput)
+            val fileNumber = VariableLengthQuantity.readVariableLengthLong(sliceInput)
+            val fileSize = VariableLengthQuantity.readVariableLengthLong(sliceInput)
+
+            val smallestKeyLength = VariableLengthQuantity.readVariableLengthInt(sliceInput)
+            val smallestKey = sliceInput.readBytes(smallestKeyLength).toInternalKey
+            val largestKeyLength = VariableLengthQuantity.readVariableLengthInt(sliceInput)
+            val largestKey = sliceInput.readBytes(largestKeyLength).toInternalKey
+
+            versionEdit.addFile(level, fileNumber, fileSize, smallestKey, largestKey)
+        }
+
+        override def writeValue(sliceOutput: SliceOutput, versionEdit: VersionEdit): Unit = {
+            import InternalKey.InternalKeyToSliceImplicit
+            versionEdit.newFiles.foreach((entry: (Int, List[FileMetaData])) => {
+                val level = entry._1
+                val fileList = entry._2
+                fileList.foreach(fileMetaData => {
+                    VariableLengthQuantity.writeVariableLengthInt(persistentId, sliceOutput)
+                    VariableLengthQuantity.writeVariableLengthInt(level, sliceOutput)
+
+                    VariableLengthQuantity.writeVariableLengthLong(fileMetaData.fileNumber, sliceOutput)
+                    VariableLengthQuantity.writeVariableLengthLong(fileMetaData.fileSize, sliceOutput)
+
+                    val smallestKeySlice = fileMetaData.smallest.toSlice
+                    val largestKeySlice = fileMetaData.largest.toSlice
+
+                    VariableLengthQuantity.writeVariableLengthInt(smallestKeySlice.length, sliceOutput)
+                    sliceOutput.writeBytes(smallestKeySlice)
+
+                    VariableLengthQuantity.writeVariableLengthInt(largestKeySlice.length, sliceOutput)
+                    sliceOutput.writeBytes(largestKeySlice)
+                })
+            })
+        }
+    }
+
+
     def getVersionEditTagByPersistentId(persistentId: Int): Option[VersionEditTag] = {
         persistentId match {
             case COMPARATOR.persistentId => Some(COMPARATOR)
--- src/test/scala/com/capslock/leveldb/VersionEditTagSpec.scala
+++ src/test/scala/com/capslock/leveldb/VersionEditTagSpec.scala
@@ -1,19 +1,111 @@
 package com.capslock.leveldb
 
+import com.capslock.leveldb.comparator.{BytewiseComparator, InternalKeyComparator}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.immutable.TreeMap
 
 /**
  * Created by capslock.
  */
 class VersionEditTagSpec extends FlatSpec with Matchers with MockFactory {
     "readValue writeValue method" should "match each other" in {
-        VersionEditTag.values.foreach(versionEditTag => {
-            testReadWrite(versionEditTag)
+        testReadWriteForComparator()
+        testLogNumber()
+        testPreLogNumber()
+        testNextFileNumber()
+        testLastSequence()
+        testCompactPointer()
+    }
+
+    def testReadWriteForComparator(): Unit = {
+        val versionEditTag = VersionEditTag.COMPARATOR
+        val versionEditForWrite = VersionEdit()
+        val comparatorName = Some("comparator")
+        versionEditForWrite.comparatorName = comparatorName
+
+        testEditTagWriteRead(versionEditTag, versionEditForWrite, versionEdit => {
+            versionEdit.comparatorName shouldEqual comparatorName
         })
     }
 
-    def testReadWrite(versionEditTag: VersionEditTag): Unit = {
+    def testLogNumber(): Unit = {
+        val versionEditTag = VersionEditTag.LOG_NUMBER
+        val versionEditForWrite = VersionEdit()
+        val logNumber = Some(1000L)
+        versionEditForWrite.logNumber = logNumber
+
+        testEditTagWriteRead(versionEditTag, versionEditForWrite, versionEdit => {
+            versionEdit.logNumber shouldEqual logNumber
+        })
     }
+
+    def testPreLogNumber(): Unit = {
+        val versionEditTag = VersionEditTag.PREVIOUS_LOG_NUMBER
+        val versionEditForWrite = VersionEdit()
+        val preLogNumber = Some(10000L)
+        versionEditForWrite.previousLogNumber = preLogNumber
+
+        testEditTagWriteRead(versionEditTag, versionEditForWrite, versionEdit => {
+            versionEdit.previousLogNumber shouldEqual preLogNumber
+        })
+    }
+
+    def testNextFileNumber(): Unit = {
+        val versionEditTag = VersionEditTag.NEXT_FILE_NUMBER
+        val versionEditForWrite = VersionEdit()
+        val nextFileNumber = Some(100000L)
+        versionEditForWrite.nextFileNumber = nextFileNumber
+
+        testEditTagWriteRead(versionEditTag, versionEditForWrite, versionEdit => {
+            versionEdit.nextFileNumber shouldEqual nextFileNumber
+        })
+    }
+
+    def testLastSequence(): Unit = {
+        val versionEditTag = VersionEditTag.LAST_SEQUENCE
+        val versionEditForWrite = VersionEdit()
+        val lastSequence = Some(100000L)
+        versionEditForWrite.lastSequenceNumber = lastSequence
+
+        testEditTagWriteRead(versionEditTag, versionEditForWrite, versionEdit => {
+            versionEdit.lastSequenceNumber shouldEqual lastSequence
+        })
+    }
+
+    def testCompactPointer(): Unit = {
+        val versionEditTag = VersionEditTag.COMPACT_POINTER
+        val versionEditForWrite = VersionEdit()
+        var compactPointers = TreeMap[Int, InternalKey]()
+        compactPointers += (1 -> InternalKey(Slice("key"), 10000L, ValueType.VALUE))
+        versionEditForWrite.compactPointers = compactPointers
+
+        testEditTagWriteRead(versionEditTag, versionEditForWrite, versionEdit => {
+            versionEdit.compactPointers.foreach {
+                case (level: Int, key: InternalKey) =>
+                    InternalKeyComparator(BytewiseComparator()).compare(compactPointers(level), key) shouldEqual 0
+            }
+        })
+    }
+
+    def testEditTagWriteRead(versionEditTag: VersionEditTag, versionEditForWrite: VersionEdit,
+                             checkFunc: (VersionEdit) => Unit): Unit = {
+        val (persistentId, resultVersionEdit) = writeEditToSliceAndReadEditFromSlice(versionEditTag, versionEditForWrite)
+        persistentId shouldEqual versionEditTag.persistentId
+        checkFunc(resultVersionEdit)
+    }
+
+    def writeEditToSliceAndReadEditFromSlice(versionEditTag: VersionEditTag, versionEdit: VersionEdit): (Int, VersionEdit) = {
+        val output = DynamicSliceOutput(10)
+        versionEditTag.writeValue(output, versionEdit)
+        val input = SliceInput(output.slice())
+
+        val resultVersionEdit = VersionEdit()
+        val persistentId = VariableLengthQuantity.readVariableLengthInt(input)
+        versionEditTag.readValue(input, resultVersionEdit)
+        (persistentId, resultVersionEdit)
+    }
+
 
 }
